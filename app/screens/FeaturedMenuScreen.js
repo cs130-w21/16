0c23b31d7e24af4853afbdd2bdff28c46cd70de3
Brendon Ng <brendonn8@gmail.com>
2021-01-19 23:50:30 -0800

    Init app structure

---
+++ app/screens/FeaturedMenuScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+import colors from '../config/colors';
+
+function FeaturedMenuScreen(props) {
+    return{
+
+    };
+}
+
+export default FeaturedMenuScreen;
