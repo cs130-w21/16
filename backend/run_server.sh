4ce8edf6eb1c4d00090092450fcdde7b39c2bb86
Brendon Ng <brendonn8@gmail.com>
2021-02-05 13:02:17 -0800

    Set up run_server.sh

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-if [$# -eq 0];
+
+if [[ $# -eq 0 ]];
 then
     docker pull brendonng/potluck:base
+    docker stop potluck-dev-server
+    docker rm potluck-dev-server
     docker run -it -p 3000:3000 --name potluck-dev-server brendonng/potluck:base
 else
     branch=$1
-    branchclean = `echo $1 | tr -dc '[:alnum:]\n\r'`
+    branchclean=`echo $1 | tr -dc '[:alnum:]\n\r'`
     DOCKER_BUILDKIT=1 docker build --build-arg branch=$branch --ssh default -t brendonng/potluck:$branchclean .
+    docker stop potluck-dev-server
+    docker rm potluck-dev-server
     docker run -it -p 3000:3000 --name potluck-dev-server brendonng/potluck:$branchclean
 fi
