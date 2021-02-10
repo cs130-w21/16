b4dbe448173abf1728b1233b395f2e93af41f26e
Brendon Ng <brendonn8@gmail.com>
2021-02-09 16:19:28 -0800

    Update server route

--- backend/routes.js
+++ backend/routes.js
@@ -63,6 +63,23 @@ app.get('/AvailableDishes', function (req, res) {
     });
 });
 
+app.get('/ChefInfo', function (req, res) {
+    // Connecting to the database.
+    connection.getConnection(function (err, connection) {
+        if(err) throw err;
+        // Executing the MySQL query (select all data from the 'users' table).
+        const chefid = req.query.id
+        connection.query('SELECT * FROM Chef WHERE chefid = '+chefid, function (error, results, fields) {
+        // If some error occurs, we throw an error.
+        if (error) throw error;
+
+        // Getting the 'response' from the database and sending it to our route. This is were the data is.
+        res.send(results)
+        });
+        connection.release();
+    });
+});
+
 
 // Starting our server.
 var args = process.argv.slice(2);
--- backend/run_server.sh
+++ backend/run_server.sh
@@ -5,12 +5,12 @@ then
     docker pull brendonng/potluck:base
     docker stop potluck-dev-server
     docker rm potluck-dev-server
-    docker run -it -p 8888:8888 --name potluck-dev-server brendonng/potluck:base
+    docker run -p 8888:8888 --name potluck-dev-server brendonng/potluck:base
 else
     branch=$1
     branchclean=`echo $1 | tr -dc '[:alnum:]\n\r'`
     DOCKER_BUILDKIT=1 docker build --build-arg branch=$branch --ssh default -t brendonng/potluck:$branchclean .
     docker stop potluck-dev-server
     docker rm potluck-dev-server
-    docker run -it -p 8888:8888 --name potluck-dev-server brendonng/potluck:$branchclean
+    docker run -p 8888:8888 --name potluck-dev-server brendonng/potluck:$branchclean
 fi
