fef31e317cc5940092adf371e9fc029ac1dcd658
Brendon Ng <brendonn8@gmail.com>
2021-02-09 22:12:54 -0800

    Cover Photo route

--- backend/routes.js
+++ backend/routes.js
@@ -80,6 +80,23 @@ app.get('/ChefInfo', function (req, res) {
     });
 });
 
+app.get('/CoverPhotos', function (req, res) {
+    // Connecting to the database.
+    connection.getConnection(function (err, connection) {
+        if(err) throw err;
+        // Executing the MySQL query (select all data from the 'users' table).
+        const chefid = req.query.id
+        connection.query('SELECT primaryImage FROM Dish WHERE chefid = '+chefid, function (error, results, fields) {
+        // If some error occurs, we throw an error.
+        if (error) throw error;
+
+        // Getting the 'response' from the database and sending it to our route. This is were the data is.
+        res.send(results)
+        });
+        connection.release();
+    });
+});
+
 
 // Starting our server.
 var args = process.argv.slice(2);
