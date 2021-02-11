afe92ccf7ba2640778965b79e86b7ec5dea08ab1
Brendon Ng <brendonn8@gmail.com>
2021-02-11 15:10:30 -0800

    New Database (#52)

--- backend/create_tables.sql
+++ backend/create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Chef(chefid INT PRIMARY KEY, name VARCHAR(100), bio TEXT, shortDesc VARCHAR(50), location BLOB, rating FLOAT, numReviews INT, profilePic TEXT);
-CREATE TABLE Dish(dishid INT PRIMARY KEY, chefid INT, name VARCHAR(100), price FLOAT, description TEXT, shortDesc VARCHAR(100), ingredients TEXT, timeMin INT, rating FLOAT, numReviews INT, primaryImage TEXT, secondImage TEXT, thirdImage TEXT, fourthImage TEXT, available BOOLEAN);
-CREATE TABLE Review(dishid INT PRIMARY KEY, chefid INT, reviewer VARCHAR(100) PRIMARY KEY, rating INT, comment TEXT, timestamp DATETIME PRIMARY KEY);
-CREATE TABLE Orders(orderid INT PRIMARY KEY, customer VARCHAR(100), dishid INT, chefid INT, placed DATETIME);
+CREATE TABLE IF NOT EXISTS Chef(chefid INT PRIMARY KEY, name VARCHAR(100), bio TEXT, shortDesc VARCHAR(50), location BLOB, rating FLOAT, numReviews INT, profilePic TEXT); 
+CREATE TABLE IF NOT EXISTS Dish(dishid INT PRIMARY KEY, chefid INT, name VARCHAR(100), price FLOAT, description TEXT, shortDesc VARCHAR(100), ingredients TEXT, timeMin INT, rating FLOAT, numReviews INT, primaryImage TEXT, secondImage TEXT, thirdImage TEXT, fourthImage TEXT, available BOOLEAN); 
+CREATE TABLE IF NOT EXISTS Orders(orderid INT PRIMARY KEY, customer VARCHAR(100), dishid INT, chefid INT, placed DATETIME);
+CREATE TABLE IF NOT EXISTS Review(dishid INT, chefid INT, reviewer VARCHAR(100), rating INT, comment TEXT, timestamp DATETIME, PRIMARY KEY (dishid, reviewer, timestamp)); 
