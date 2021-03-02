272d18183cea465fa4f7074d780c9a9e1a01c97a
Brendon Ng <brendonn8@gmail.com>
2021-03-01 19:40:39 -0800

    fixed directory location of id_rsa

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)"
-chmod 600 .travis/id_rsa
-ssh-add .travis/id_rsa
+chmod 600 id_rsa
+ssh-add id_rsa
 
 git config --global push.default matching
 git remote add deploy ssh://ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com
