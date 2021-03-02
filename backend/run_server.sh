c4bc1a5d82488552c9960b872b9e77c044d0dae9
Brendon Ng <brendonn8@gmail.com>
2021-03-01 20:00:42 -0800

    logfiles

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -5,7 +5,7 @@ then
     docker pull brendonng/potluck:master
     docker stop potluck-dev-server
     docker rm potluck-dev-server
-    docker run -p 8888:8888 --name potluck-dev-server brendonng/potluck:master
+    docker run -p 8888:8888 --name potluck-dev-server brendonng/potluck:master > ~/docker8888.logfile
 else
     branch=$1
     branchclean=`echo $1 | tr -dc '[:alnum:]\n\r'`
@@ -14,5 +14,5 @@ else
     DOCKER_BUILDKIT=1 docker build --build-arg branch=$branch -t brendonng/potluck:$branchclean .
     docker stop potluck-test-server
     docker rm potluck-test-server
-    docker run -p 8080:8080 --name potluck-test-server brendonng/potluck:$branchclean
+    docker run -p 8080:8080 --name potluck-test-server brendonng/potluck:$branchclean > ~/docker8080.logfile
 fi
