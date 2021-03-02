6887358c695c206fb433b6e72978569dc33e73f8
Brendon Ng <brendonn8@gmail.com>
2021-03-01 23:44:44 -0800

    EOF issues

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,21 +16,23 @@ ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amaz
     cd backend
     ./run_server.sh > ~/logfiles/run_server8888.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
 EOF
+
 else
-    count=`wget https://raw.githubusercontent.com/cs130-w21/16/dev/backend/routes.js -O - | diff backend/routes.js - | wc -l`
-    if [[ $count -gt 0 ]]; then
-        eval "$(ssh-agent -s)"
-        chmod 600 id_rsa
-        ssh-add id_rsa
+count=`wget https://raw.githubusercontent.com/cs130-w21/16/dev/backend/routes.js -O - | diff backend/routes.js - | wc -l`
+if [[ $count -gt 0 ]]; then
+eval "$(ssh-agent -s)"
+chmod 600 id_rsa
+ssh-add id_rsa
 
-        git config --global push.default matching
-        git remote add deploy ssh://ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com
-        git push deploy dev
+git config --global push.default matching
+git remote add deploy ssh://ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com
+git push deploy dev
 
-        ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com <<EOF
-            cd ~/16
-            cd backend
-            ./run_server.sh $1 > ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
-        EOF
-    fi
+ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com <<EOF
+    cd ~/16
+    cd backend
+    touch $1
+    ./run_server.sh $1 > ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
+EOF
+fi
 fi
