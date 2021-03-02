f6f653bac95ad49bba3df408f3ef4bbef804e630
Brendon Ng <brendonn8@gmail.com>
2021-03-02 11:56:29 -0800

    fix hanging

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,7 +13,7 @@ ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amaz
     git pull origin dev
     cd backend
     echo "./run_server.sh at `date "+%Y.%m.%d-%H.%M.%S"`" >> ~/logfiles/run_servers.logfile
-    ./run_server.sh 2> ~/logfiles/run_server8888.`date "+%Y.%m.%d-%H.%M.%S"`.logfile 1> ~/logfiles/run_server8888.`date "+%Y.%m.%d-%H.%M.%S"`.logfile
+    ./run_server.sh 2> ~/logfiles/run_server8888.`date "+%Y.%m.%d-%H.%M.%S"`.logfile 1> ~/logfiles/run_server8888.`date "+%Y.%m.%d-%H.%M.%S"`.logfile;
     echo "server ran for dev at `date "+%Y.%m.%d-%H.%M.%S"`" >> ~/logfiles/run_servers.logfile
 EOF
 
@@ -36,7 +36,7 @@ ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amaz
     cd ~/16
     cd backend
     echo "./run_server.sh $branch at `date "+%Y.%m.%d-%H.%M.%S"`" >> ~/logfiles/run_servers.logfile
-    ./run_server.sh $branch 2> ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile 1> ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile
+    ./run_server.sh $branch 2> ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile 1> ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile;
     echo "server ran for $branch at `date "+%Y.%m.%d-%H.%M.%S"`" >> ~/logfiles/run_servers.logfile
 EOF
 fi
