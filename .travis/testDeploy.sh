7afbca6ee4e791441a8063c5da88229128611628
Brendon Ng <brendonn8@gmail.com>
2021-03-01 23:26:55 -0800

    directory bug 2

--- .travis/testDeploy.sh
+++ .travis/testDeploy.sh
@@ -4,5 +4,5 @@ ls -a
 count=`wget https://raw.githubusercontent.com/cs130-w21/16/dev/backend/routes.js -O - | diff backend/routes.js - | wc -l`
 echo $count
 if [[ $count -gt 0 ]]; then
-    ./runTestOnServer.sh $1
+    .travis/runTestOnServer.sh $1
 fi
