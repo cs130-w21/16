88d00f39264a9858ea6b2a8f5ba17984bf6ca851
Brendon Ng <brendonn8@gmail.com>
2021-03-01 23:03:39 -0800

    test deploy'

---
+++ .travis/testDeploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+count=`wget https://raw.githubusercontent.com/cs130-w21/16/dev/backend/routes.js -O - | diff ../backend/routes.js - | wc -l`
+if [[ $count -gt 0 ]]; then
+    eval "$(ssh-agent -s)"
+    chmod 600 id_rsa
+    ssh-add id_rsa
+
+    git config --global push.default matching
+    git remote add deploy ssh://ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com
+    git push deploy dev
+
+    ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com <<EOF
+        cd ~/16
+        cd backend
+        touch $1.txt
+        ./run_server.sh $1 > ~/logfiles/run_server8888.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
+    EOF
+fi
