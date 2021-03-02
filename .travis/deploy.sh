5dbd16c3867618d8000cd201fad72a17a1b8e0a5
Brendon Ng <brendonn8@gmail.com>
2021-03-01 23:51:48 -0800

    should perform normally

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-if [$# -eq 1]; then
+echo $#
+if [$# -le 1]; then
 eval "$(ssh-agent -s)"
 chmod 600 id_rsa
 ssh-add id_rsa
@@ -28,11 +28,12 @@ git config --global push.default matching
 git remote add deploy ssh://ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com
 git push deploy dev
 
+branch=$1
 ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com <<EOF
     cd ~/16
     cd backend
-    touch $1
-    ./run_server.sh $1 > ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
+    touch "$branch".txt
+    ./run_server.sh $branch > ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
 EOF
 fi
 fi
