3633b04c7b8c70cb4fab729b0363f912ba12bc0e
Brendon Ng <brendonn8@gmail.com>
2021-03-01 19:29:58 -0800

    deploy script debuginmg messages

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,9 +8,11 @@ git config --global push.default matching
 git remote add deploy ssh://ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com
 git push deploy dev
 
+echo "running outside"
 ssh ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com <<EOF
+    echo "running"
     cd ~/16
-    mkdir success
+    touch success
     git pull origin dev
     cd backend
     node routes.js
