d7668c4a129d63f830eb75b504001b8c119763d1
Brendon Ng <brendonn8@gmail.com>
2021-03-01 19:47:18 -0800

    runs server

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,11 +8,8 @@ git config --global push.default matching
 git remote add deploy ssh://ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com
 git push deploy dev
 
-echo "running outside"
 ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com <<EOF
-    echo "running"
     cd ~/16
-    touch success
     git pull origin dev
     cd backend
     ./run_server.sh
