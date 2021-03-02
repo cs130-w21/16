daa34172de3e8eaabc57cedd99a1f9f48e7caad5
Brendon Ng <brendonn8@gmail.com>
2021-03-01 23:16:02 -0800

    dummy change

--- .travis/runTestOnServer.sh
+++ .travis/runTestOnServer.sh
@@ -12,5 +12,5 @@ ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amaz
     cd ~/16
     cd backend
     touch $1.txt
-    ./run_server.sh $1 > ~/logfiles/run_server8888.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
+    ./run_server.sh $1 > ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
 EOF
