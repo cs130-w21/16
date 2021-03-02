70337604736b1f95460fd0a42282e115706ce3f0
Brendon Ng <brendonn8@gmail.com>
2021-03-01 23:30:12 -0800

    changes to runTestONSever

--- .travis/runTestOnServer.sh
+++ .travis/runTestOnServer.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)"
-chmod 600 id_rsa
-ssh-add id_rsa
 
-git config --global push.default matching
-git remote add deploy ssh://ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com
-git push deploy dev
 
 ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com <<EOF
     cd ~/16
