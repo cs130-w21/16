ed617c707b978dd1ad8fd19865f73177f6c1da48
Brendon Ng <brendonn8@gmail.com>
2021-03-02 10:30:03 -0800

    CICD full functionality: test branches (#91)
    * travis scripts
    
    * fixed fiel path of id_rsa.enc
    
    * correct directrory for travis encryption
    
    * saved .travis.yml
    
    * travis attempt again
    
    * travis -r
    
    * travis --com
    
    * deploy script debuginmg messages
    
    * ls to debug
    
    * fixed directory error
    
    * made deploy.sh executable
    
    * fixed directory location of id_rsa
    
    * dont ask for permission on ssh
    
    * runs server
    
    * runs server
    
    * halts execution
    
    * runs to logfile
    
    * logfiles
    
    * only runs on certain branches
    
    * remove id_rsa.pub from .gitignore
    
    * test deploy'
    
    * runs test Deploy
    
    * Executables
    
    * dummy change
    
    * dummy change
    
    * debugging message
    
    * directory bug
    
    * directory bug 2
    
    * changes to runTestONSever
    
    * try again
    
    * all in deploy.sh
    
    * EOF issues
    
    * argument number mistake:
    
    * should perform normally
    
    * if statment fixes
    
    * force test branch
    
    * added new travis.yml
    
    * removed quotes
    
    * should take branch 2
    
    * Test test server
    
    * same routes.js as dev
    
    * print count for deploy
    
    * routes the same as dev
    
    * Working functionality: opens new instance on 8080 test server if routes.js is changed on any other branch that is not main

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+if [[ $# -lt 1 ]]; then
 eval "$(ssh-agent -s)"
 chmod 600 id_rsa
 ssh-add id_rsa
@@ -14,3 +14,24 @@ ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amaz
     cd backend
     ./run_server.sh > ~/logfiles/run_server8888.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
 EOF
+
+else
+count=`wget https://raw.githubusercontent.com/cs130-w21/16/dev/backend/routes.js -O - | diff backend/routes.js - | wc -l`
+echo $count
+if [[ $count -gt 0 ]]; then
+eval "$(ssh-agent -s)"
+chmod 600 id_rsa
+ssh-add id_rsa
+
+git config --global push.default matching
+git remote add deploy ssh://ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com
+git push deploy dev
+
+branch=$1
+ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-141-20-190.us-east-2.compute.amazonaws.com <<EOF
+    cd ~/16
+    cd backend
+    ./run_server.sh $branch > ~/logfiles/run_server8080.`date "+%Y.%m.%d-%H.%M.%S"`.logfile &
+EOF
+fi
+fi
