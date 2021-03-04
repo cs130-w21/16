1fc0e3d064e3b9f896cabce0971262ca8c5c57ac
Nikhil <nikhil.srikumar@gmail.com>
2021-03-04 06:59:16 -0800

    Fixed merge conflictss in NavBar

--- app/components/NavBarComponent.js
+++ app/components/NavBarComponent.js
@@ -1,56 +1,69 @@
-import React, { Component, View } from "react";
+import React, { Component, useCallback, useEffect, useState, View } from "react";
 import { Header, Icon, Badge } from "react-native-elements";
 import { Text, StyleSheet, TouchableOpacity } from "react-native";
 import { cartSum } from "./ShoppingCart";
 import colors from "../config/colors";
 
-class RightElement extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { sum: cartSum() };
-  }
+function RightElement(props) {
+  const [sum, setSum] = useState(cartSum());
 
-  render() {
-    const { navigation, search } = this.props;
-    if (search == null)
-    {
-      return (
-        <TouchableOpacity
-          onPress={() => {
+  const {navigation, search} = props;
+  if (search)  //if its on the search screen the right element has to be the map icon
+  {
+    return (
+      <Icon
+        name="location-pin"
+        type="simple-line-icon"
+        color="white"
+        onPress={() => {
+          navigation.navigate("MapScreen");
+        }}
+      />
+    )
+  }
+  else
+  {
+
+    return (
+      !global.orderOpen ? 
+      <TouchableOpacity
+        onPress={() => {
+          if(global.orderOpen){
+            navigation.navigate("Tracking");
+          } else {
             navigation.navigate("ShoppingCart");
-            this.setState({ sum: cartSum() });
-          }}
-        >
-          <Icon name="shopping-cart" color="white" />
-          {cartSum() > 0 ? (
-            <Badge
+          }
+          setSum(cartSum());      }}
+      >
+        <Icon name="shopping-cart" color="white" />
+        {cartSum() > 0 ? (
+          <Badge
+            containerStyle={{ position: "absolute", top: -4, right: -4 }}
+            badgeStyle={{ backgroundColor: colors.secondary }}
+          />
+        ) : (
+          <></>
+        )}
+      </TouchableOpacity>
+      :
+      <TouchableOpacity
+        onPress={() => {
+          if(global.orderOpen){
+            navigation.navigate("Tracking");
+          } else {
+            navigation.navigate("ShoppingCart");
+          }
+        }}
+      >
+        <Icon name='shopping-cart' color='white'/>
+        <Badge
               containerStyle={{ position: "absolute", top: -4, right: -4 }}
               badgeStyle={{ backgroundColor: colors.secondary }}
-            />
-          ) : (
-            <></>
-          )}
-        </TouchableOpacity>
-        
-      );
-    }
-    else
-    {
-      return (
-        <Icon
-          name="location-pin"
-          type="simple-line-icon"
-          color="white"
-          onPress={() => {
-            navigation.navigate("MapScreen");
-          }}
         />
-      );
+      </TouchableOpacity>
+    );
+  }
 
-    }
-
-    }
-    
 }
 
 class LeftElement extends Component {
@@ -87,28 +100,31 @@ class CenterElement extends Component {
   }
 }
 
-class NavBarComponent extends Component {
-  render() {
-    return (
-      <Header
-        containerStyle={{ backgroundColor: colors.primary }}
-        leftComponent={
-          <LeftElement
-            navigation={this.props.navigation}
-            search={this.props.search}
-          />
-        }
-        centerComponent={<CenterElement />}
-        rightComponent={
-          <RightElement 
-            navigation={this.props.navigation}
-            search={this.props.search} 
-          />
-        }
-        barStyle="light-content"
-      />
-    );
-  }
+function NavBarComponent(props){
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    setRefresh(!refresh);
+  }, [global.cart])
+
+  useEffect(() => {
+    setRefresh(!refresh);
+  }, [global.orderOpen])
+
+  return (
+    <Header
+      containerStyle={{ backgroundColor: colors.primary }}
+      leftComponent={
+        <LeftElement
+          navigation={props.navigation}
+          search={props.search}
+        />
+      }
+      centerComponent={<CenterElement />}
+      rightComponent={<RightElement navigation={props.navigation} search={props.search} />}
+      barStyle="light-content"
+    />
+  );
 }
 
 const styles = StyleSheet.create({
--- app/screens/CheckoutScreen.js
+++ app/screens/CheckoutScreen.js
@@ -3,48 +3,50 @@ import { Alert, View, SafeAreaView } from "react-native";
 import { StyleSheet } from "react-native";
 import { Card, Button, Input } from "react-native-elements";
 import CheckoutSubtotal from "../components/checkoutSubtotal";
-import { getDishes, isolateChefs } from "../components/ShoppingCart";
+import { dishesPerChef, getDishes, isolateChefs } from "../components/ShoppingCart";
 import colors from "../config/colors";
 import { sendTextMessage } from "../util/Queries";
-//const secrets = require("../../backend/secrets");
+import Order from '../objects/Order';
+const secrets = require("../../backend/secrets");
 
 function CheckoutScreen(props) {
-  const [comment, setComment] = useState("");
+  const [comment, setComment] = useState({});
   const navigation = props.navigation;
+  function setCommentChef(thisComment, chef){
+    var comments = comment;
+    comments[chef] = thisComment;
+    setComment(comments);
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <CheckoutSubtotal></CheckoutSubtotal>
-      <View>
-        <Input
-          placeholder="Special Instructions"
-          placeholderTextColor="grey"
-          multiline={true}
-          numberOfLines={5}
-          containerStyle={styles.commentContainer}
-          inputContainerStyle={styles.commentInputContainer}
-          inputStyle={styles.commentInput}
-          onChangeText={text => {
-            setComment(text);
-          }}
-        />
-      </View>
-      <View style={styles.paymentTab}>
+    <View style={styles.container}>
+      <CheckoutSubtotal setComment={setCommentChef}></CheckoutSubtotal>
+      <SafeAreaView style={styles.paymentTab}>
         <Button
           type="solid"
           title="Pay with Cash"
           titleStyle={styles.paymentText}
           buttonStyle={styles.paymentButton}
+          containerStyle={styles.paymentButtonContainer}
           onPress={() => {
             // Do a thing
-            var pairs = isolateChefs();
-            Object.keys(pairs).forEach(chef =>
+            if(!global.orderOpen){
+              global.orderOpen = true;
+              global.progress = {};
+              global.orders = new Order(dishesPerChef());
+              var pairs = isolateChefs();
+              Object.keys(pairs).forEach(chef =>
               sendTextMessage(
                 getDishes(pairs[chef]),
-                comment != "" ? comment : "None",
+                comment[chef] != "" ? comment[chef] : "None",
                 secrets.phone(chef),
                 "Cash"
               ).then(navigation.navigate("Tracking"))
-            );
+              );
+            } else {
+              global.orderOpen = true;
+              navigation.navigate("Tracking")
+            }
           }}
         />
         <Button
@@ -52,31 +54,40 @@ function CheckoutScreen(props) {
           title="Pay with Venmo"
           titleStyle={styles.paymentText}
           buttonStyle={styles.paymentButton}
+          containerStyle={styles.paymentButtonContainer}
           onPress={() => {
             // Do a thing
-            var pairs = isolateChefs();
-            Object.keys(pairs).forEach(chef =>
-              sendTextMessage(
-                getDishes(pairs[chef]),
-                comment != "" ? comment : "None",
-                secrets.phone(chef),
-                "Venmo"
-              ).then(navigation.navigate("Tracking"))
-            );
+            if(!global.orderOpen){
+              global.orderOpen = true;
+              global.progress = {};
+              global.orders = new Order(dishesPerChef());
+              var pairs = isolateChefs();
+              Object.keys(pairs).forEach(chef =>
+                sendTextMessage(
+                  getDishes(pairs[chef]),
+                  comment[chef] != "" ? comment[chef] : "None",
+                  secrets.phone(chef),
+                  "Venmo"
+                ).then(navigation.navigate("Tracking"))
+              );
+            } else {
+              global.orderOpen = true;
+              navigation.navigate("Tracking")
+            }
           }}
         />
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.background
+    backgroundColor: 'white',
+    height: '100%'
   },
   paymentTab: {
     flex: 1,
-    backgroundColor: colors.primary,
     justifyContent: "space-evenly",
     alignContent: "center",
     alignItems: "center",
@@ -93,6 +104,10 @@ const styles = StyleSheet.create({
   paymentButton: {
     borderRadius: 20,
     backgroundColor: colors.secondary
+  },
+  paymentButtonContainer: {
+    width: '40%',
+    marginBottom: 20
   },
   commentInputContainer: {
     // height: "100%"
